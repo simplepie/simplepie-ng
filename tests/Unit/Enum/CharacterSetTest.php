b48230b78450372b0ad0df47480b0e4bf5470704
Ryan Parman <ryan@ryanparman.com>
2017-07-01 14:13:15 -0700

    Added the very first tests.

---
+++ tests/Unit/Enum/CharacterSetTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright (c) 2017 Ryan Parman <http://ryanparman.com>.
+ * Copyright (c) 2017 Contributors.
+ *
+ * http://opensource.org/licenses/Apache2.0
+ */
+declare(strict_types=1);
+
+namespace SimplePie\Test\Unit\Enum;
+
+use SimplePie\Enum\CharacterSet;
+use SimplePie\Test\Unit\AbstractTestCase;
+
+class CharacterSetTest extends AbstractTestCase
+{
+    public function testIntrospect()
+    {
+        $this->assertEquals(CharacterSet::introspect(), [
+            'ISO_8859_1' => 'iso-8859-1',
+            'US_ASCII'   => 'us-ascii',
+            'UTF_8'      => 'utf-8',
+            'WIN_1252'   => 'windows-1252',
+        ]);
+    }
+
+    public function testIntrospectKeys()
+    {
+        $this->assertEquals(CharacterSet::introspectKeys(), [
+            'ISO_8859_1',
+            'US_ASCII',
+            'UTF_8',
+            'WIN_1252',
+        ]);
+    }
+
+    public function testHasValue()
+    {
+        $this->assertTrue(CharacterSet::hasValue(CharacterSet::ISO_8859_1));
+        $this->assertTrue(CharacterSet::hasValue(CharacterSet::US_ASCII));
+        $this->assertTrue(CharacterSet::hasValue(CharacterSet::UTF_8));
+        $this->assertTrue(CharacterSet::hasValue(CharacterSet::WIN_1252));
+
+        $this->assertFalse(CharacterSet::hasValue('nope'));
+    }
+}
