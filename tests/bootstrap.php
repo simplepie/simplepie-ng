1d34d3bf13630972f5b07737dd1dbd3c459ee133
Ryan Parman <ryan@ryanparman.com>
2017-07-07 00:47:51 -0700

    Add a space after the header comment, but before the declare(strict_types=1).

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Copyright (c) 2017 Ryan Parman <http://ryanparman.com>.
+ * Copyright (c) 2017 Contributors.
+ *
+ * http://opensource.org/licenses/Apache2.0
+ */
 
 declare(strict_types=1);
 error_reporting(-1);
